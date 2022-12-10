32b4b2d3c6f701b4cfe1ba40b4d5df28f9c140f5
Dipta-Sarker <contact.diptasarker@gmail.com>
2022-12-10 17:20:29 +0600

    create fake json data

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const app = express()
 var cors = require('cors')
 const port = 5000
 
+const courses = require('./data/courses.json')
+
 app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/courses', (req,res) =>{
+  res.send(courses)
 })
 
 app.listen(port, () => {
